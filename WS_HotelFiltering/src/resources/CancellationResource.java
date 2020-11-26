027a86c717a9b3c5d24d2e4bed8e137fefd61ed5
Drindrin77 <leiina77410@gmail.com>
2020-11-26 15:00:34 +0100

    add libs and delete request

--- WS_HotelFiltering/src/resources/CancellationResource.java
+++ WS_HotelFiltering/src/resources/CancellationResource.java
@@ -6,6 +6,7 @@ import org.restlet.data.Form;
 import org.restlet.data.MediaType;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Delete;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
@@ -14,19 +15,19 @@ import helper.DateHelper;
 
 public class CancellationResource extends ServerResource{
 	
-	@Post
+	@Delete
 	public Representation cancelBooking(Representation entity) {  
 		Representation result = null;  
 
-		Form form = new Form(entity);
-        int idClient = Integer.parseInt(form.getFirstValue("idClient"));  
-        int idHotel = Integer.parseInt(form.getFirstValue("idHotel"));  
-        Date arrivalDate = DateHelper.stringToDate(form.getFirstValue("arrivalDate"));  
-        Date endDate = DateHelper.stringToDate(form.getFirstValue("endDate"));  
-        int nbRoom = Integer.parseInt(form.getFirstValue("nbRoom"));      
+		int nbRoom = Integer.parseInt(getQuery().getValues("nbRoom"));  
+        int idClient = Integer.parseInt(getQuery().getValues("idClient"));  
+        int idHotel = Integer.parseInt(getQuery().getValues("idHotel")); 
+        Date arrivalDate = DateHelper.stringToDate(getQuery().getValues("arrivalDate"));
+        Date endDate = DateHelper.stringToDate(getQuery().getValues("endDate")); 
+           
         
         if(HotelManager.cancelBooking(idHotel, idClient, arrivalDate, endDate, nbRoom)) {
-        	result = new StringRepresentation("true",  
+        	result = new StringRepresentation("true",
                     MediaType.TEXT_PLAIN);         
         }else {
         	result = new StringRepresentation("false",  
